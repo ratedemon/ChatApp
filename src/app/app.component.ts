f3f10446f711388562b4ff20c4bbdc1ff8427e08
ratedemon <rated_demon@ukr.net>
2017-06-30 18:56:27 +0300

    add logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, ViewChild, AfterViewChecked, ElementRef } from '@angular/core';
+import { Component, OnInit, Output, ViewChild, } from '@angular/core';
 import {DataService} from './shared/data.service';
 import {Observable} from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
@@ -14,45 +14,17 @@ import {TranslateService} from '@ngx-translate/core';
   styleUrls: ['./app.component.css']
 })
 
-export class AppComponent implements OnInit, AfterViewChecked{
-  // title: string = "ChatApp";
-  year = new Date().getFullYear();
-  user: any;
-  isMain: boolean = false;
-  @ViewChild('sendMessage') private myScrollContainer: ElementRef; 
-  constructor(private dataService: DataService, private route: ActivatedRoute, private router: Router, private loginService: LoginService, private translate: TranslateService){
+export class AppComponent implements OnInit{
+  user: any;  
+  constructor(private dataService: DataService, private router: Router, private loginService: LoginService, private translate: TranslateService){
     translate.addLangs(['en','ru']);
 
     translate.setDefaultLang('en');
-    translate.use('ru');
+    translate.use('en');
     let browserLang = translate.getBrowserLang();
-    console.log(browserLang);
-    router.events.subscribe((url:any) => {if(url.url == '/' && url.url.length < 2){
-      this.isMain = true
-    }else{
-      this.isMain = false;
-    }
-});
   }
   ngOnInit(){
     this.dataService.initUser().subscribe(data=>{this.user = data; console.log(this.user)});
-    if(this.router.url == '/'){
-      this.isMain = !this.isMain;
-    }
-    else{
-      this.isMain = false;
-    }
-    this.scrollToBottom();
-  }
-  
-
-  ngAfterViewChecked():void{
-    this.scrollToBottom();
-  }
-  scrollToBottom(){
-    if(this.router.url=="/chat"){
-      this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;      
-    }
   }
   logout(){
     this.loginService.logout();
